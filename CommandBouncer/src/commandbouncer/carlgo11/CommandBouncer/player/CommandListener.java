8b5481461d4fbc1e1d9e103ce4b679e02f6ee920
Carlgo11 <Carlgo11@portalcraft.se>
2013-06-08 17:05:05 +0200

    Fixed the onPlayerCommand not acting on cmds bug, and more...

--- CommandBouncer/src/commandbouncer/carlgo11/CommandBouncer/player/CommandListener.java
+++ CommandBouncer/src/commandbouncer/carlgo11/CommandBouncer/player/CommandListener.java
@@ -23,26 +23,66 @@ public class CommandListener implements Listener {
     @EventHandler
     public void onPlayerCommand(PlayerCommandPreprocessEvent e) {
         System.out.println("chat registred!");
+        System.out.println("a = " + CommandBouncer.a);
+        System.out.println("b = " + CommandBouncer.b);
         Player player = e.getPlayer();
         String cmd = e.getMessage();
         final String[] asd = e.getMessage().split(" ");
 
-        for (int a = 1; a < CommandBouncer.b; a++) {
+        for (int a = 1; a < CommandBouncer.b; a++) { // cmdbnc.a ! b
+            if (plugin.getConfig().getBoolean("debug") == true) {
             System.out.println("forloop started!");
-            if (e.getMessage().equalsIgnoreCase(plugin.getConfig().getString("cmd"+a))) {
-                System.out.println("matches cmd"+a);
-                if (player.hasPermission("CommandBouncer.listen.cmd" + a) || player.hasPermission("CommandBouncer.listen.*") || player.hasPermission("CommandBouncer.*")) {
-                    if (plugin.getConfig().getString("ignore-worlds").equalsIgnoreCase(player.getWorld() + "")) {
-                        if (plugin.getConfig().contains("console" + a)) {
-                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfig().getString("console" + a));
-                        }
-                        if (plugin.getConfig().contains("player" + a)) {
-                            Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), plugin.getConfig().getString("player" + a));
-                        }
+            }
+            if (e.getMessage().equalsIgnoreCase("/" + plugin.getConfig().getString("cmd" + a))) {
+                if (plugin.getConfig().getBoolean("debug") == true) {
+                System.out.println("matches cmd" + a);
+                }
 
+
+                if (player.hasPermission("CommandBouncer.listen.cmd" + a) || player.hasPermission("CommandBouncer.listen.*") || player.hasPermission("CommandBouncer.*")) {
+                    //if (!plugin.getConfig().getString("ignore-worlds").equalsIgnoreCase(player.getWorld() + "")) {
+                    if (plugin.getConfig().contains("console" + a)) {
+                        String dastring = plugin.getConfig().getString("console" + a);
+                        String replaceAlla = dastring.replaceAll("%player%", player.getName());
+                        if (plugin.getConfig().getBoolean("debug") == true) {
+                        System.out.println("dastring:" + replaceAlla);
+                        System.out.println("Console bnc string found!");
+                        }
+                        Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), replaceAlla);
+                        if (plugin.getConfig().getBoolean("debug") == true) {
+                        System.out.println("Console" + a + ": " + plugin.getConfig().getString("Console" + a));
+                        }
+                    } else {
+                        if (plugin.getConfig().getBoolean("debug") == true) {
+                        System.out.println("No console bnc string found!");
+                        }
                     }
+                    if (plugin.getConfig().contains("player" + a)) {
+                        String dastring = plugin.getConfig().getString("player" + a);
+                        String replaceAlla = dastring.replaceAll("%player%", player.getName());
+                            System.out.println("dastring:" + dastring);
+                        System.out.println("player bnc string found!");
+                        Bukkit.getServer().dispatchCommand(Bukkit.getPlayer(e.getPlayer().getName()), replaceAlla);
+                    } else {
+                        if (plugin.getConfig().getBoolean("debug") == true) {
+                        System.out.println("No player bnc string found!");
+                        }
+                    }
+
+                    //}
+                }
+            } else {
+                if (plugin.getConfig().getBoolean("debug") == true) {
+                    System.out.println("No match: cmd" + a);
+                    System.out.println("player's-cmd:" + e.getMessage());
+                    System.out.println("cmd" + a + ":" + plugin.getConfig().getString("cmd" + a));
+                    System.out.println(" ");
                 }
             }
         }
+    }
+
+    private void replaceAll(String player, String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
