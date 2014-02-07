45bd1dd3049785686ffe32c6b58075f6b201ae4f
Carlgo11 <Carlgo11@portalcraft.se>
2014-02-07 15:33:42 +0100

    Cleanup

--- src/main/java/com/carlgo11/CommandBouncer/CommandBouncer.java
+++ src/main/java/com/carlgo11/CommandBouncer/CommandBouncer.java
@@ -1,11 +1,11 @@
 package com.carlgo11.CommandBouncer;
 
+import com.carlgo11.CommandBouncer.Commands.*;
 import com.carlgo11.CommandBouncer.updater.Updater;
 import com.carlgo11.CommandBouncer.player.CommandListener;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -19,11 +19,17 @@ public class CommandBouncer extends JavaPlugin {
         checkcmd();
         checkConfig();
         checkUpdate();
+        commands();
         getServer().getPluginManager().registerEvents(new CommandListener(this), this);
     }
 
     public void onDisable() {
-        this.getLogger().info(getDescription().getName() + " v"+getDescription().getVersion() + " is disalbed.");
+        this.getLogger().info(getDescription().getName() + " v"+getDescription().getVersion() + " is disabled.");
+    }
+    
+    public void commands()
+    {
+        getCommand("simpleautomessage").setExecutor(new CommandBouncerCommand(this));
     }
 
     public void checkConfig() {
@@ -45,68 +51,7 @@ public class CommandBouncer extends JavaPlugin {
 
 
 
-    @Override
-    public boolean onCommand(final CommandSender sender, Command cmd, String commandLabel, String[] args) {         //Used when we wan't to add a command fairly :P
-        String prefix = ChatColor.GREEN + "[" + getDescription().getName() + "] " + ChatColor.RESET;
-
-        if (cmd.getName().equalsIgnoreCase("CommandBouncer")) {
-            if (args.length == 0) { // EXAMPLE: /cmd arg0 arg1 arg2
-                if (sender.hasPermission("commandbouncer.cmd.commandbouncer") || sender.hasPermission("CommandBouncer.*")) {
-                    sender.sendMessage(ChatColor.YELLOW + "============ " + ChatColor.GREEN + getDescription().getName() + ChatColor.YELLOW + " ============");
-                    sender.sendMessage("");
-                    sender.sendMessage(ChatColor.GRAY + "-  /" + ChatColor.RED + "CommandBouncer" + ChatColor.YELLOW + " Shows all avible commands");
-                    sender.sendMessage(ChatColor.GRAY + "-  /" + ChatColor.RED + "CommandBouncer Reload" + ChatColor.YELLOW + " Reload the config.yml");
-                    sender.sendMessage(ChatColor.GRAY + "-  /" + ChatColor.RED + "CommandBouncer List" + ChatColor.YELLOW + " List all the commands that the plugin listens on");
-                } else {
-                    this.badpermsSender(sender);
-                }
-            } else if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("reload")) {
-                    if (sender.hasPermission("commandbouncer.cmd.commandbouncer.reload") || sender.hasPermission("CommandBouncer.*")) {
-                        getServer().getPluginManager().disablePlugin(this);
-                        getServer().getPluginManager().enablePlugin(this);
-                        sender.sendMessage(prefix + ChatColor.GREEN + "Config reloaded!");
-                    } else {
-                        this.badpermsSender(sender);
-                    }
-                }
-
-                if (args[0].equalsIgnoreCase("list")) {
-                    if (sender.hasPermission("commandbouncer.cmd.commandbouncer.list") || sender.hasPermission("CommandBouncer.*")) {
-                        sender.sendMessage(ChatColor.YELLOW + "============ " + ChatColor.GREEN + prefix + ChatColor.YELLOW + " ============");
-                        sender.sendMessage(ChatColor.GRAY+"Gray = Command to listen to.");
-                        sender.sendMessage(ChatColor.BLUE+"Blue = Console command.");
-                        sender.sendMessage(ChatColor.RED+"Red = Player command.");
-                        int d = 1;
-                        int e = 0;
-                        int f = 1;
-                        StringBuilder message = new StringBuilder();
-                        for (d = d; e != f; d++) {
-                            if (getConfig().contains("cmd" + d)) {
-                                message.append("\n" + ChatColor.YELLOW + "cmd" + d + ": " + ChatColor.GRAY);
-                                message.append("/" + getConfig().getString("cmd" + d)+"");
-                                if (getConfig().contains("console" + d)) {
-                                    message.append(ChatColor.RESET+" " + ChatColor.BLUE +"/"+ getConfig().getString("console" + d));
-                                }
-                                if (getConfig().contains("player" + d)) {
-                                    message.append(ChatColor.RESET+" " + ChatColor.RED +"/"+ getConfig().getString("player" + d));
-                                }
-                            } else {
-                                e++;
-                            }
-                        }
-                        sender.sendMessage("" + message);
-                    } else {
-                        this.badpermsSender(sender);
-                    }
-                    // more 1 arg commands here:
-                }
-            } else if (args.length == 2) {
-                // Add code here?
-            }
-        }
-        return true;
-    }
+    
     public static int a = 1; // cmd checker
     public static int b = 0; // Error checker
     public static int c = 3; // Max errors allowed int
